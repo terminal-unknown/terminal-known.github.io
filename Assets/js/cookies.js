d313c989ac96bdf88f6dd5b929c2ea6825abfebe
Elena Miller <emiller242@my.stlcc.edu>
2022-04-13 23:12:33 -0500

    css styles update

--- Assets/js/cookies.js
+++ Assets/js/cookies.js
@@ -1,7 +1,8 @@
 // JavaScript source code
 function writeCookie() {
+
     var cookievalue = escape(prompt("Type in a value to store in a cookie"));
-    document.cookie = cookievalue;
+    document.cookie = "name=" + cookievalue;
 
     document.write();
 }
